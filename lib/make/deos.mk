edd32621638c2e1b7761b822ce27445a946510d9
Andrew DeSantis <atd@bitcoin.sh>
2017-07-03 23:53:32 -0500

    swap: lib <-> src

---
+++ lib/make/deos.mk
@@ -0,0 +1,4 @@
+include lib/make/deos-boolean.mk
+include lib/make/deos-git.mk
+include lib/make/deos-host.mk
+include lib/make/deos-unix.mk
--- src/make/deos.mk
+++
@@ -1,4 +0,0 @@
-include src/make/deos-boolean.mk
-include src/make/deos-git.mk
-include src/make/deos-host.mk
-include src/make/deos-unix.mk
